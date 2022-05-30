c392dbb34fb199e05f083e784ae0af3491c9e2e0
Сергей <7atom7@mail.ru>
2022-05-30 10:38:42 +0300

    Произведен рефакторинг, функция сохранения изображения преобразована, теперь возможно сохранить несколько изображений из json ответа с сайта SpaceX

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import requests
 from pathlib import Path
 
 
-def load_images(image_url, image_path):
-    filename = 'hubble.jpeg'
-    Path(image_path).mkdir(parents=True, exist_ok=True)
-    response = requests.get(image_url)
+def fetch_spacex_last_launch(content_url, content_path):
+    Path(content_path).mkdir(parents=True, exist_ok=True)
+    response = requests.get(content_url)
     response.raise_for_status()
-
-    with open(os.path.join(image_path, filename), 'wb') as file:
-        file.write(response.content)
+    count = 0
+    for i in response.json()['links']['flickr']['original']:
+        filename = f'space_{count}.jpg'
+        count +=1
+        with open(os.path.join(content_path, filename), 'wb') as file:
+            file.write(requests.get(i).content)
 
 if __name__ == '__main__':
-    image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    image_path = 'images'
-    load_images(image_url, image_path)
+    flight_id = '5eb87d47ffd86e000604b38a'
+    content_url = f'https://api.spacexdata.com/v4/launches/{flight_id}'
+    content_path = 'images'
+    fetch_spacex_last_launch(content_url, content_path)
