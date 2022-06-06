31fc8f3bbcc48feebf1b25a2f5e4db144b0015fd
Сергей <7atom7@mail.ru>
2022-06-06 11:42:37 +0300

    Добавлена функция nasa_epic_images, она выводит ссылку на картинку на наса эпик

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from importlib.resources import path
 import os
+import datetime
 from random import randint
 from urllib.parse import urlparse, unquote
 import requests
@@ -16,13 +17,13 @@ def saving_images(content, content_path):
             file.write(requests.get(i).content)
         count+=1
 
-
 def fetch_spacex_last_launch(content_url):
     params = {}
     response = requests.get(content_url, params=params)
     response.raise_for_status()
+    filtered_response = response.json()['links']['flickr']['original']
     spacex_images_database = []
-    for i in response.json()['links']['flickr']['original']:
+    for i in filtered_response:
         spacex_images_database.append(i)
     return spacex_images_database
 
@@ -39,12 +40,25 @@ def nasa_space_img(nasa_token):
         nasa_images_database.append(i['url'])
     return nasa_images_database
 
-
 def define_image_format(test_url):
     image_name = os.path.split(urlparse(test_url).path)
     image_format = os.path.splitext(unquote(image_name[-1]))
     return image_format
 
+def nasa_epic_images(nasa_token):
+    epic_url = 'https://api.nasa.gov/EPIC/api/natural/images'
+    params = {
+        'api_key': f'{nasa_token}',
+    }
+    response = requests.get(epic_url,  params=params)
+    image_datetime, image_name = response.json()[-1]['date'], response.json()[-1]['image']
+    image_date = datetime.datetime.strptime(image_datetime, '%Y-%m-%d %H:%M:%S')
+    main_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date.strftime("%Y/%m/%d")}/png/{image_name}.png'
+    params_main = {
+        'api_key': f'{nasa_token}',
+    }
+    response_main = requests.get(main_url,  params=params_main)
+    print (response_main.url)
 
 if __name__ == '__main__':
     flight_id = '5eb87d47ffd86e000604b38a'
@@ -52,7 +66,8 @@ if __name__ == '__main__':
     content_path = 'images'
     load_dotenv()
     nasa_token = os.getenv('NASA_TOKEN')
-    nasa_images = nasa_space_img(nasa_token)
+    # nasa_space_img(nasa_token)
     # print(define_image_format(nasa_url))
-    fetch_spacex_last_launch(content_url)
+    # fetch_spacex_last_launch(content_url)
     # saving_images(nasa_images, content_path)
+    nasa_epic_images(nasa_token)
